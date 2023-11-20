cb8cb8b0387040bf83e108fbf4deaf56f4350621
Artem Burashnikov <artem.burashnikov@gmail.com>
2023-11-20 23:12:00 +0300

    Add cli

--- Parsers/Program.fs
+++ Parsers/Program.fs
@@ -1,12 +1,52 @@
-﻿open Parsers.AST
+﻿open System.IO
+open Argu
 open Parsers.Combinators
 open Parsers.Interpreter
 
-let res = run Parsers.ExprParser.parseProgram "x=if(10=3)then(105)else(3)
-print:x"
+type CliArguments =
+    | [<Unique; Mandatory; AltCommandLine("-i")>] TextFile of path: string
 
-match res with
-| None -> printfn "Incorrect input"
-| Some(_, ast) ->
-    printfn $"%A{res}"
-    evalProgram ast
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | TextFile _ -> "specify a path to the file"
+
+module Main =
+
+    [<EntryPoint>]
+    let main (argv: string array) =
+
+        let errorHandler =
+            ProcessExiter(
+                colorizer =
+                    function
+                    | ErrorCode.HelpText -> None
+                    | _ -> Some System.ConsoleColor.Blue
+            )
+
+        let parser = ArgumentParser.Create<CliArguments>(errorHandler = errorHandler)
+
+        let run () =
+
+            let results = parser.ParseCommandLine argv
+
+            let filePath =
+                match results.TryGetResult(TextFile) with
+                | Some path when (File.Exists path) && (Path.GetExtension path = ".txt") -> path
+                | _ ->
+                    eprintfn "incorrect file path"
+                    exit 1
+
+            let programCode = File.ReadAllText filePath |> (fun x -> x.Trim())
+
+            let result = run Parsers.ExprParser.parseProgram programCode
+
+            match result with
+            | None ->
+                eprintfn "incorrect file path"
+                exit 1
+            | Some(_, ast) -> evalProgram ast
+
+        run ()
+
+        0
